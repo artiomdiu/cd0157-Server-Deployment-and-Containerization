bd296c05fb1d92b4c91062ecf0fb64f92278956a
AD <artiomd@gmail.com>
2022-03-24 11:28:04 +0200

    Moved JWT token to OS env

--- test_main.py
+++ test_main.py
@@ -7,14 +7,15 @@ import pytest
 
 import main
 
-SECRET = 'TestSecret'
+# SECRET = 'TestSecret'
 TOKEN = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE1NjEzMDY3OTAsIm5iZiI6MTU2MDA5NzE5MCwiZW1haWwiOiJ3b2xmQHRoZWRvb3IuY29tIn0.IpM4VMnqIgOoQeJxUbLT-cRcAjK41jronkVrqRLFmmk'
 EMAIL = 'wolf@thedoor.com'
 PASSWORD = 'huff-puff'
 
 @pytest.fixture
 def client():
-    os.environ['JWT_SECRET'] = SECRET
+    # os.environ['JWT_SECRET'] = SECRET
+    JWT_SECRET = os.environ.get('JWT_SECRET', 'abc123abc1234')
     main.APP.config['TESTING'] = True
     client = main.APP.test_client()
 
