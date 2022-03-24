b76169368007293290e37c563ad5d106e9276f5d
AD <artiomd@gmail.com>
2022-03-24 12:13:23 +0200

    Deleted test mail as os env

--- test_main.py
+++ test_main.py
@@ -9,9 +9,9 @@ import main
 
 # SECRET = 'TestSecret'
 TOKEN = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE1NjEzMDY3OTAsIm5iZiI6MTU2MDA5NzE5MCwiZW1haWwiOiJ3b2xmQHRoZWRvb3IuY29tIn0.IpM4VMnqIgOoQeJxUbLT-cRcAjK41jronkVrqRLFmmk'
-# EMAIL = 'wolf@thedoor.com'
+EMAIL = 'wolf@thedoor.com'
 # PASSWORD = 'huff-puff'
-EMAIL = os.environ.get('TEST_MAIL', 'defaulttestmail')
+# EMAIL = os.environ.get('TEST_MAIL', 'defaulttestmail')
 PASSWORD = os.environ.get('TEST_MAIL_PSW', 'defaulttestmailpsw')
 
 @pytest.fixture
